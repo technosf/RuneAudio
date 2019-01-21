5a37d810d54653909e33c3a1f197365128af4344
rern <rernrern@gmail.com>
2019-01-21 10:56:42 +0700

    Update and rename disable.sh to set.sh

---
+++ set_wlan-bt/set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm $0
+
+. /srv/http/addonstitle.sh
+
+if [[ $1 == 'yes' ]]; then
+	title -l '=' "$bar Disable WiFi and Bluetooth ..."
+
+	systemctl disable netctl-auto@wlan0
+	systemctl stop netctl-auto@wlan0 shairport upmpdcli
+	rfkill block 0
+	sed -i '/blacklist/ s/^#//' /etc/modprobe.d/disable_rpi3_wifi_bt.conf
+
+	title -nt "$info WiFi and Bluetooth disabled"
+else
+	title -l '=' "$bar Enable WiFi and Bluetooth ..."
+
+	sed -i '/blacklist/ s/^/#/' /etc/modprobe.d/disable_rpi3_wifi_bt.conf
+	systemctl enable netctl-auto@wlan0
+	systemctl start netctl-auto@wlan0 shairport upmpdcli
+
+	title -nt "$info WiFi and Bluetooth enabled."
+fi
