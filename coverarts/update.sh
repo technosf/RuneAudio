f09f07cb7a4bfbca28aec750a97922c8c0024f22
rern <rernrern@gmail.com>
2019-02-19 14:12:21 +0700

    Update update.sh

--- coverarts/update.sh
+++ coverarts/update.sh
@@ -4,17 +4,18 @@ rm $0
 
 . /srv/http/addonstitle.sh
 
-title -l '=' "$bar Update / Create coverarts for browsing ..."
-timestart
-
 if ! find /mnt/MPD -name 'coverarts' -type d; then
 	title "$info Directory 'coverarts' not exist."
 	exit
 fi
 
+title -l '=' "$bar Update / Create coverarts for browsing ..."
+
+timestart
+
 albums=$( mpc stats | grep Albums | awk '{ print $NF }' )
 minutes=(( $album / 5 ))
-echo -e "$bar This may take up to $minutes minutes ..."
+echo -e "$bar This may take up to $minutes minutes for $albums albums ..."
 
 wgetnc https://github.com/rern/RuneAudio/raw/master/coverarts/enhancecoverart.php
 chmod +x enhancecoverart.php
