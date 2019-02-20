7979170f93acdbf6a95f5e3e00a9af120761246e
rern <rernrern@gmail.com>
2019-02-20 21:23:56 +0700

    Update update.sh

--- coverarts/update.sh
+++ coverarts/update.sh
@@ -5,28 +5,9 @@ rm $0
 . /srv/http/addonstitle.sh
 
 path=$( redis-cli get pathcoverarts )
-if [[ ! $path || ! -e $path ]]; then
-	title "$info Create coverarts directory ..."
-
-	df=$( df )
-	dfUSB=$( echo "$df" | grep '/mnt/MPD/USB' | head -n1 )
-	dfNAS=$( echo "$df" | grep '/mnt/MPD/NAS' | head -n1 )
-	pathcoverarts=/mnt/MPD/LocalStorage/coverarts
-	if [[ $dfUSB ]]; then
-		mnt=$( echo $dfUSB | awk '{ print $NF }' )
-		pathcoverarts=$mnt/coverarts
-	elif [[ $dfNAS ]]; then
-		mnt=$( echo $dfNAS | awk '{ print $NF }' )
-		acl=$( getfacl $mnt | grep user | cut -d':' -f3 )
-		[[ ${acl:0:2} == rw ]] && pathcoverarts=$mnt/coverarts
-	fi
-	mkdir -p $pathcoverarts
-	pathlink=/srv/http/assets/img/coverarts
-	ln -sf $path /srv/http/assets/img/coverarts
-	chown http:http $pathcoverarts $pathlink
-	chmod 644 $pathcoverarts pathlink
-
-	redis-cli set pathcoverarts $pathcoverarts$path
+if [[ ! -e $path ]]; then
+	title "$info Thumnail directory, $path, not exist."
+	exit
 fi
 
 title -l '=' "$bar Update / Create thumbnails for browsing by coverarts..."
