14f0dcb3530b6c3c5a1a6ecfe9e6aabc4f5ee9f6
rern <rernrern@gmail.com>
2019-02-20 19:37:43 +0700

    Update update.sh

--- coverarts/update.sh
+++ coverarts/update.sh
@@ -6,8 +6,27 @@ rm $0
 
 path=$( redis-cli get pathcoverarts )
 if [[ ! -e $path ]]; then
-	title "$info Directory: $path not exist."
-	exit
+	title "$info Create coverarts directory ..."
+
+	df=$( df )
+	dfUSB=$( echo "$df" | grep '/mnt/MPD/USB' | head -n1 )
+	dfNAS=$( echo "$df" | grep '/mnt/MPD/NAS' | head -n1 )
+	pathcoverarts=/mnt/MPD/LocalStorage/coverarts
+	if [[ $dfUSB ]]; then
+		mnt=$( echo $dfUSB | awk '{ print $NF }' )
+		pathcoverarts=$mnt/coverarts
+	elif [[ $dfNAS ]]; then
+		mnt=$( echo $dfNAS | awk '{ print $NF }' )
+		acl=$( getfacl $mnt | grep user | cut -d':' -f3 )
+		[[ ${acl:0:2} == rw ]] && pathcoverarts=$mnt/coverarts
+	fi
+	mkdir -p $pathcoverarts
+	pathlink=/srv/http/assets/img/coverarts
+	ln -sf $path /srv/http/assets/img/coverarts
+	chown http:http $pathcoverarts $pathlink
+	chmod 644 $pathcoverarts pathlink
+
+	redis-cli set pathcoverarts $pathcoverarts$path
 fi
 
 title -l '=' "$bar Update / Create thumbnails for browsing by coverarts..."
