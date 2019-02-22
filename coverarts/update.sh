98828b70a748652e96957aff38c5513e4bf50269
rern <rernrern@gmail.com>
2019-02-22 18:12:43 +0700

    Update update.sh

--- coverarts/update.sh
+++ coverarts/update.sh
@@ -10,24 +10,72 @@ if [[ ! -e $pathcoverarts ]]; then
 	exit
 fi
 
+function createThumbnail() {
+	percent=$(( $i * 100 / $count ))
+	echo -e "\n${percent}% $i/$count - $( tcolor "$album" ) • $artist"
+	
+	dir=$( dirname "$file" )
+	thumbname=${thumbname//\//|} # slash "/" character not allowed in filename
+	thumbfile="$pathcoverarts/$thumbname.jpg"
+	
+	if [[ -e "$thumbfile" ]]; then
+		(( exist++ ))
+		echo "  Thumbnail already exists."
+		return 0
+	fi
+	
+	for cover in "${coverfiles[@]}"; do
+		coverfile="$dir/$cover"
+		if [[ -e "$coverfile" ]]; then
+			convert "$coverfile" \
+				-thumbnail 200x200 \
+				-unsharp 0x.5 \
+				"$thumbfile"
+			(( thumb++ ))
+			echo -e "  $pad Thumbnail created from file: $coverfile"
+			return 0
+		fi
+	done
+	
+	if [[ !$cue ]]; then
+		coverfile=$( /srv/http/enhanceID3cover.php "$file" )
+		if [[ $coverfile != 0 ]]; then
+			convert "$coverfile" -thumbnail 200x200 -unsharp 0x.5 "$thumbfile"
+			rm "$coverfile"
+			echo -e "  $pad Thumbnail created from embedded ID3: $file"
+			(( thumb++ ))
+			return 0
+		fi
+	fi
+	
+	convert /srv/http/assets/img/cover-dummy.svg \
+		-resize 200x200 \
+		-font /srv/http/assets/fonts/lato/lato-regular-webfont.ttf \
+		-pointsize 16 \
+		-fill "#e0e7ee" \
+		-annotate +10+90 "$album\n$artist" \
+		"$thumbfile"
+	echo -e "  $pad4 Coverart not found. Dummy thumbnail created."
+	(( dummy++ ))
+}
+
 title -l '=' "$bar Update / Create thumbnails for browsing by coverarts..."
 
 timestart
 
-coverfiles=( cover.png cover.jpg folder.png folder.jpg front.png front.jpg Cover.png Cover.jpg Folder.png Folder.jpg Front.png Front.jpg )
 exist=0
 thumb=0
 dummy=0
 pad=$( tcolor '.' 6 6 )
 pad4=$( tcolor '.' 4 4 )
-
+coverfiles=( cover.png cover.jpg folder.png folder.jpg front.png front.jpg Cover.png Cover.jpg Folder.png Folder.jpg Front.png Front.jpg )
 
 # get album
 listalbum=$( mpc list album | awk NF )
 readarray -t albums <<<"$listalbum"
 
 # get album artist - expand albums with same name
-echo -e "$bar Get album list ...\n"
+title "$bar Get album list ..."
 count=${#albums[@]}
 i=0
 albumArtist=
@@ -36,73 +84,49 @@ for album in "${albums[@]}"; do
 	albumArtist="$albumArtist"$'\n'"$find"
 	(( i++ ))
 	percent=$(( $i * 100 / $count ))
-	echo "${percent}% - $album"
+	echo "${percent}% $i/$count - $album"
 done
 readarray -t albumArtists <<<"${albumArtist:1}" # remove 1st \n
 echo -e "\n$( tcolor $( numfmt --g $i ) ) Album names"
 
-echo -e "\n\n$bar Get album-artist list ...\n"
+title "$bar Get album-artist list ..."
 # get album artist file
 count=${#albumArtists[@]}
 i=0
-albumArtistFile=
 for albumArtist in "${albumArtists[@]}"; do
 	album=$( echo "$albumArtist" | cut -d'^' -f1 )
 	artist=$( echo "$albumArtist" | cut -d'^' -f2 )
 	filempd=$( mpc find -f %file% album "$album" artist "$artist" | head -n1 )
 	file=/mnt/MPD/$filempd
-	dir=$( dirname "$file" )
-	thumbname=${albumArtist//\//|} # slash "/" character not allowed in filename
-	thumbname=${albumArtist/^/^^} # slash "/" character not allowed in filename
-	thumbfile=$pathcoverarts/$thumbname.jpg
+	thumbname="$album^^$artist"
 	(( i++ ))
-	percent=$(( $i * 100 / $count ))
-	echo -e "\n${percent}% $i/$count - $( tcolor "$album" ) • $artist"
-	if [[ -e $thumbfile ]]; then
-		echo "  Thumbnail already exists."
-		(( exist++ ))
-		continue
-	fi
-	created=0
-	for cover in "${coverfiles[@]}"; do
-		coverfile=$dir/$cover
-		if [[ -e $coverfile ]]; then
-			convert "$coverfile" \
-				-thumbnail 200x200 \
-				-unsharp 0x.5 \
-				"$thumbfile"
-			(( thumb++ ))
-			created=1
-			echo -e "  $pad Thumbnail created from file: $coverfile"
-			break
-		fi
-	done
-	[[ $created ]] && continue
-	
-	coverfile=$( /srv/http/enhanceID3cover.php "$file" )
-	if [[ $coverfile != 0 ]]; then
-		convert "$coverfile" -thumbnail 200x200 -unsharp 0x.5 "$thumbfile"
-		rm "$coverfile"
-		echo -e "  $pad Thumbnail created from embedded ID3: $file"
-		(( thumb++ ))
-		continue
-	fi
-	anotate="$album\n$artist\n$filempd"
-	convert /srv/http/assets/img/cover-dummy.svg \
-		-resize 200x200 \
-		-font /srv/http/assets/fonts/lato/lato-regular-webfont.ttf \
-		-pointsize 16 \
-		-fill "#e0e7ee" \
-		-annotate +10+85 "$anotate" \
-		"$thumbfile"
-	echo -e "  $pad4 Coverart not found. Dummy thumbnail created."
-	(( dummy++ ))
+	createThumbnail
 done
+countalbum=$i
+
+# cue - not in mpd database
+title "$bar Cue Sheet - Get album list ..."
+
+cueFiles=$( find /mnt/MPD -type f -name '*.cue' )
+readarray -t files <<<"$cueFiles"
+count=${#files[@]}
+cue=1
+i=0
+for file in "${files[@]}"; do
+	tag=$( cat "$file" | grep '^TITLE\|^PERFORMER' )
+	album=$( echo "$tag" | grep TITLE | sed 's/.*"\(.*\)".*/\1/' )
+	artist=$( echo "$tag" | grep PERFORMER | sed 's/.*"\(.*\)".*/\1/' )
+	filempd=${file/\/mnt\/MPD\/}
+	thumbname="$album^^$artist^^$filempd"
+	(( i++ ))
+	createThumbnail
+done
+countalbum=$(( $countalbum + $i ))
 
 echo -e "\nNew thumbnails      : $( tcolor $( numfmt --g $thumb ) )"
 (( $dummy )) && echo -e "Dummy thumbnails    : $( tcolor $( numfmt --g $dummy ) )"
 (( $exist )) && echo -e "Existing/Duplicates : $( tcolor $( numfmt --g $exist ) )"
-echo -e "Albums              : $( tcolor $( numfmt --g $i ) )"
+echo -e "Albums              : $( tcolor $( numfmt --g $countalbum ) )"
 
 # save album count
 redis-cli set countalbum $i &> /dev/null
@@ -111,7 +135,7 @@ curl -s -v -X POST 'http://localhost/pub?id=notify' -d '{ "title": "'"Coverart B
 
 timestop
 
-title "$bar Thumbnails updated / created successfully."
+title -l '=' "$bar Thumbnails updated / created successfully."
 echo -e "$bar To change:"
 echo "  - Coverart files used before ID3 embedded"
 echo "  - Replace coverart normally and update"
