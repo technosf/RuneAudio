9497c60b3c32c6517ade9e6afc41f448dff39372
rern <rernrern@gmail.com>
2019-03-11 18:57:26 +0700

    Update set.sh

--- set_screenoff/set.sh
+++ set_screenoff/set.sh
@@ -7,7 +7,7 @@ rm $0
 title -l '=' "$bar Set screen off timeout ..."
 
 export DISPLAY=:0
-xset dpms $(( $1 * 60 )) 0 0
+xset dpms 0 0 $(( $1 * 60 ))
 
 (( $1 == 0 )) && set=$( tcolor disabled ) || set="set to $( tcolor $1 ) minutes"
 
