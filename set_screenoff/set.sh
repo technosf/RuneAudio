41366dc46e6a0a88e6db0f9aad2f5ed27a10f977
rern <rernrern@gmail.com>
2019-03-11 16:59:28 +0700

    Update set.sh

--- set_screenoff/set.sh
+++ set_screenoff/set.sh
@@ -4,11 +4,11 @@ rm $0
 
 . /srv/http/addonstitle.sh
 
-title -l '=' "$bar Change screen off duration ..."
+title -l '=' "$bar Set screen off timeout ..."
 
 export DISPLAY=:0
 xset s $(( $1 * 60 )) 0
 
-(( $1 == 0 )) && min=disabled || min="$1 minutes"
+(( $1 == 0 )) && min=$( tcolor disabled ) || min="set to $( tcolor $1 ) minutes"
 
-title -nt "$info Screen off timeout changed to $( tcolor "$min" )"
+title -nt "$info Screen off timeout $min"
