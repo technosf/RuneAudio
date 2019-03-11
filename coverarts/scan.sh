705333ef8d1f8de3e0782884355a488bbffce832
rern <rernrern@gmail.com>
2019-03-11 18:41:27 +0700

    Update scan.sh

--- coverarts/scan.sh
+++ coverarts/scan.sh
@@ -120,19 +120,9 @@ function createThumbnail() {
 		fi
 	fi
 	
-	[[ -n $artist ]] && anotate="$album\n$artist" || anotate=$album
-	convert /srv/http/assets/img/cover-dummy.svg \
-		-resize 200x200 \
-		-font /srv/http/assets/fonts/lato/lato-regular-webfont.ttf \
-		-pointsize 16 \
-		-fill "#e0e7ee" \
-		-annotate +10+90 "$anotate" \
-		"$thumbfile"
-	echo -e "$padB Coverart not found. Dummy thumbnail created."
+	ln -s /srv/http/assets/img/cover.svg "${thumbfile:0:-3}svg"
+	echo -e "$padB Coverart not found."
 	(( dummy++ ))
-	#ln -s /srv/http/assets/img/cover.svg "${thumbfile:0:-3}svg"
-	#echo -e "$padB Coverart not found."
-	#(( dummy++ ))
 }
 
 [[ $( redis-cli exists countalbum ) == 1 ]] && update=Update || update=Create
