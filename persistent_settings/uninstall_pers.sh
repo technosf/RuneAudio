a807b4045cc6510c05103600dfb0b1e47a44fd88
rern <rernrern@gmail.com>
2019-04-09 12:24:23 +0700

    Update and rename persistent_seettings/uninstall_pers.sh to persistent_settings/uninstall_pers.sh

--- persistent_settings/uninstall_pers.sh
+++ persistent_settings/uninstall_pers.sh
@@ -9,16 +9,20 @@ title -l '=' "$bar Restore database and settings from persistent storage ..."
 pathsettings=$( dirname $( readlink -f /etc/netctl ) )
 
 removeDirLink() { # $1-pathlink $2-chown
-  rm -rf $1
-  cp -rf "$pathsettings/$( basename $1 )" $( dirname $1 )
-  [[ -n $2 ]] && chown -R $2 $1
+	echo -e "$bar $1"
+	rm -rf $1
+	cp -rf "$pathsettings/$( basename $1 )" $( dirname $1 )
+	[[ -n $2 ]] && chown -R $2 $1
 }
-echo -e "$bar /etc/netctl"
+
 removeDirLink /etc/netctl
-echo -e "$bar /var/lib/redis"
-removeDirLink /var/lib/redis redis:redis
-echo -e "$bar /var/lib/mpd"
+
 removeDirLink /var/lib/mpd mpd:audio
 mv -f /var/lib/mpd/mpd.conf /etc # restore changes
+
+redis-cli save &> /dev/null
+systemctl stop redis
+removeDirLink /var/lib/redis redis:redis
+systemctl start redis
 
 title -nt "$bar Database and settings restore successfully."
