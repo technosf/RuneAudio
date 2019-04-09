c528047eea6866387d5d808dc66b745cde67cc66
rern <rernrern@gmail.com>
2019-04-09 10:46:05 +0700

    Update install.sh

--- persistent_seettings/install.sh
+++ persistent_seettings/install.sh
@@ -12,22 +12,25 @@ if [[ ! -L $imgsettings ]]; then
 	echo -e "$info No persistent storage found/writable in USB/NAS"
 	rm -rf $imgsettings
 	exit
-else
-	pathsettings=$( readlink -f $imgsettings )
-	rm -rf $imgsettings
-	moveDirLink() { # $1-pathold $2-chown
-		dirold=$( basename $1 )
-		pathnew=$pathsettings/$dirold
-		[[ ! -e "$pathnew" ]] && mv $1 "$pathsettings"
-		rm -rf $1
-		ln -s "$pathnew" $( dirname $1 )
-		[[ -n $2 ]] && chown -R $2 "$pathnew" $1
-	}
-	moveDirLink /etc/netctl
-	moveDirLink /var/lib/redis redis:redis
-	moveDirLink /var/lib/mpd mpd:audio
-	[[ ! -e "$pathsettings/mpd.conf" ]] && cp /etc/mpd.conf "$pathnew" # maintain changes
-	ln -sf "$pathnew/mpd.conf" /etc
 fi
+
+pathsettings=$( readlink -f $imgsettings )
+rm -rf $imgsettings
+moveDirLink() { # $1-pathold $2-chown
+	dirold=$( basename $1 )
+	pathnew=$pathsettings/$dirold
+	[[ ! -e "$pathnew" ]] && mv $1 "$pathsettings"
+	rm -rf $1
+	ln -s "$pathnew" $( dirname $1 )
+	[[ -n $2 ]] && chown -R $2 "$pathnew" $1
+}
+echo -e "$bar /etc/netctl"
+moveDirLink /etc/netctl
+echo -e "$bar /var/lib/redis"
+moveDirLink /var/lib/redis redis:redis
+echo -e "$bar /var/lib/mpd"
+moveDirLink /var/lib/mpd mpd:audio
+[[ ! -e "$pathsettings/mpd.conf" ]] && cp /etc/mpd.conf "$pathnew" # maintain changes
+ln -sf "$pathnew/mpd.conf" /etc
 
 title -nt "$info database and settings moved to: $( tcolor "$pathsettings" )"
