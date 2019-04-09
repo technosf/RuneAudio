7dab64e1d05212258bcafc03c0c642eeb7f531c8
rern <rernrern@gmail.com>
2019-04-09 13:34:34 +0700

    Update uninstall_pers.sh

--- persistent_settings/uninstall_pers.sh
+++ persistent_settings/uninstall_pers.sh
@@ -6,15 +6,18 @@ alias=pers
 
 title -l '=' "$bar Restore database and settings from persistent storage ..."
 
+if [[ ! -L /etc/netctl ]]; then
+	echo -e "$info No persistent database and settings found."
+	exit
+fi
+
 pathsettings=$( dirname $( readlink -f /etc/netctl ) )
 
 removeDirLink() { # $1-pathlink $2-chown
-	echo -e "$bar $1"
 	rm -rf $1
 	cp -rf "$pathsettings/$( basename $1 )" $( dirname $1 )
 	[[ -n $2 ]] && chown -R $2 $1
 }
-
 removeDirLink /etc/netctl
 
 removeDirLink /var/lib/mpd mpd:audio
@@ -23,6 +26,6 @@ mv -f /var/lib/mpd/mpd.conf /etc # restore changes
 redis-cli save &> /dev/null
 systemctl stop redis
 removeDirLink /var/lib/redis redis:redis
-systemctl start redis
+systemctl restart redis rune_SY_wrk rune_PL_wrk
 
 title -nt "$bar Database and settings restore successfully."
