8e599c96d2ae48eae5132aec384db8cce49d8f2e
rern <rernrern@gmail.com>
2019-04-13 10:27:37 +0700

    Add files via upload

--- coverarts/scandir.sh
+++ coverarts/scandir.sh
@@ -3,7 +3,7 @@
 [[ -n "$1" ]] && scanpath=$1
 [[ -n "$2" ]] && removeexist=$2
 
-rm $0
+#rm $0
 
 . /srv/http/addonstitle.sh
 
@@ -16,64 +16,6 @@ padW=$( tcolor '.' 7 7 )
 padC=$( tcolor '.' 6 6 )
 padB=$( tcolor '.' 4 4 )
 padR=$( tcolor '.' 1 1 )
-coverfiles='cover.jpg cover.png folder.jpg folder.png front.jpg front.png Cover.jpg Cover.png Folder.jpg Folder.png Front.jpg Front.png'
-
-function createThumbnail() {
-	percent=$(( $i * 100 / $count ))
-	echo
-	echo ${percent}% $( tcolor "$i/$count" 8 ) $( tcolor "$thumbname" )
-	
-	# "/" not allowed in filename, "#" and "?" not allowed in img src
-	thumbname=$( echo $thumbname | sed 's|/|\||g; s/#/{/g; s/?/}/g' )
-	thumbfile="$pathcoverarts/$thumbname.jpg"
-	
-	if [[ ! -v removeexist && -e "$thumbfile" ]]; then
-		(( exist++ ))
-		echo "  Skip - Thumbnail exists."
-		return
-	fi
-	
-	for cover in $coverfiles; do
-		coverfile="$dir/$cover"
-		if [[ -e "$coverfile" ]]; then
-			convert "$coverfile" \
-				-thumbnail 200x200 \
-				-unsharp 0x.5 \
-				"$thumbfile"
-			if [[ $? == 0 ]]; then
-				echo -e "$padC Thumbnail created - file: $coverfile"
-				(( thumb++ ))
-				return
-			fi
-		fi
-	done
-	
-	find=$( find "$dir" -maxdepth 1 -type f )
-	if [[ -n $find ]]; then
-		readarray -t files <<<"$find"
-		for file in "${files[@]}"; do
-			mime=$( file -b --mime-type $file | cut -c1-5 )
-			if [[ $mime == audio ]]; then
-				coverfile=$( /srv/http/enhanceID3cover.php "$file" )
-				if [[ $coverfile != 0 ]]; then
-					convert "$coverfile" -thumbnail 200x200 -unsharp 0x.5 "$thumbfile"
-					if [[ $? == 0 ]]; then
-						rm "$coverfile"
-						echo -e "$padC Thumbnail created - ID3: $file"
-						(( thumb++ ))
-						return
-					fi
-					rm "$coverfile"
-				fi
-			fi
-		done
-	fi
-	
-	ln -s /srv/http/assets/img/cover.svg "${thumbfile:0:-3}svg"
-	echo -e "$padB Coverart not found."
-	(( dummy++ ))
-	return
-}
 
 [[ -n $( ls /srv/http/assets/img/coverarts ) ]] && update=Update || update=Create
 coloredname=$( tcolor 'Browse By Directory CoverArt' )
@@ -81,50 +23,88 @@ coloredname=$( tcolor 'Browse By Directory CoverArt' )
 title -l '=' "$bar $update thumbnails for $coloredname ..."
 
 echo -e "$bar Update Library database ..."
-mpc update | head -n1
 
 title "$bar Get directory list ..."
 
 [[ -v scanpath ]] && path=$1 || path=/mnt/MPD
+imgcoverarts=/srv/http/assets/img/coverarts
 find=$( find "$path" -type d )
 if [[ -z $find ]]; then
-	title "$info No music files found in $1"
+	title "$info No directories found in $1"
 	exit
 fi
+
+# create temporary file to let bash run php with arguments
+cat << 'EOF' > /tmp/scandir.php
+#!/usr/bin/php
+<?php
+require_once( '/srv/http/enhancegetcover.php' );
+echo getThumbnail( $argv[ 1 ], 'scancover' );
+EOF
+chmod +x /tmp/scandir.php
+
+/srv/http/enhancegetcover.php
+
 readarray -t dirs <<<"$find"
 count=${#dirs[@]}
 echo -e "\n$( tcolor $( numfmt --g $count ) ) Directories"
 i=0
 for dir in "${dirs[@]}"; do
-	thumbname=${dir/\/mnt\/MPD\/}
 	(( i++ ))
-	createThumbnail
+	percent=$(( $i * 100 / $count ))
+	echo
+	mpdpath=${dir:9}
+	echo ${percent}% $( tcolor "$i/$count" 8 ) $( tcolor "$mpdpath" )
+	coverfile=$( /tmp/scandir.php "$dir" )
+	pathname=${mpdpath//\//|}
+	thumbfile=$imgcoverarts/${mpdpath//\//|}.jpg
+	if [[ ! -v removeexist && -e "$thumbfile" ]]; then
+		(( exist++ ))
+		echo "  Skip - Thumbnail exists."
+		return
+	fi
+	
+	created=0
+	if [[ $coverfile != 0 ]]; then
+		echo $coverfile
+		echo $thumbfile
+#		convert "$coverfile" -thumbnail 200x200 -unsharp 0x.5 "$thumbfile"
+		if [[ $? == 0 ]]; then
+			chown http:http "$thumbfile"
+			echo -e "$padC Thumbnail created."
+			(( thumb++ ))
+			created=1
+		fi
+	fi
+	if [[ ! $created ]]; then
+		thumbfile=${thumbfile:0:-3}svg
+#		ln -s /srv/http/assets/img/cover.svg "$thumbfile"
+		chown -h http:http "$thumbfile"
+		echo -e "$padB Coverart not found."
+		(( dummy++ ))
+	fi
 done
 
-chown -R http:http "$pathcoverarts" /srv/http/assets/img/coverarts
+rm /tmp/scandir.php
 
-echo -e "\n\n$padC New thumbnails     : $( tcolor $( numfmt --g $thumb ) )"
-(( $dummy )) && echo -e "$padB Dummy thumbnails   : $( tcolor $( numfmt --g $dummy ) )"
-(( $exist )) && echo -e "Existings thumbnails : $( tcolor $( numfmt --g $exist ) )"
-if [[ -v scanpath ]]; then
-	echo -e "Update directory     : $( tcolor "$( basename "$scanpath" )" )"
-fi
+echo -e              "\n\n$padC New thumbnails     : $( tcolor $( numfmt --g $thumb ) )"
+(( $dummy )) && echo -e  "$padB Dummy thumbnails   : $( tcolor $( numfmt --g $dummy ) )"
+(( $exist )) && echo -e      "Existings thumbnails : $( tcolor $( numfmt --g $exist ) )"
+[[ -v scanpath ]] && echo -e "Update directory     : $( tcolor "$scanpath" )"
 
-curl -s -v -X POST 'http://localhost/pub?id=notify' -d '{ "title": "'"Coverart Browsing"'", "text": "'"Thumbnails updated."'" }' &> /dev/null
+curl -s -v -X POST 'http://localhost/pub?id=notify' \
+	-d '{ "title": "'"Browse By CoverArt"'", "text": "'"Thumbnails ${update}d."'" }' \
+	&> /dev/null
 
 timestop
 
 title -l '=' "$bar Thumbnails for $coloredname ${update}d successfully."
 
-if [[ $( echo $pathcoverarts | cut -d'/' -f4 ) == LocalStorage ]]; then
-	echo -e "$info $( tcolor $pathcoverarts ) is in SD card. Backup before reflash."
-fi
 echo
-echo Thumbnails directory : $( tcolor "$pathcoverarts" )
+echo Thumbnails directory : $( tcolor "$imgcoverarts" )
 echo
-echo -e "$bar To change :"
-echo "    - Replace - manage coverarts normally and update (Coverart files used before ID3 embedded)"
-echo "    - Delete  - long-press on each thumbnail"
-echo -e "$bar To update :"
-echo "    - Full    - Library > long-press $( tcolor CoverArt )"
-echo "    - Partial - Library > directory > Context menu > Update thumbnails"
+echo -e "$bar To change individually:"
+echo "    - CoverArt > long-press thumbnail > Delete / Replace icon"
+echo -e "$bar To update with database:"
+echo "    - Full    - Library > long-press CoverArt"
+echo "    - Partial - Library > directory > context menu > Update thumbnails"
