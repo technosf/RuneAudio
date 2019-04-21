243b86f8d5ab78d4e9a3fe014511f89722f271f2
rern <rernrern@gmail.com>
2019-04-21 18:40:24 +0700

    Add files via upload

--- coverarts/scan.sh
+++ coverarts/scan.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 path=$1
-[[ $2 != 0 ]] && removeexist=1
+[[ $2 == 1 ]] && removeexist=1
 
-rm $0
+#rm $0
 
 . /srv/http/addonstitle.sh
 
-[[ $3 != 0 ]] && mpc update
+[[ $3 == 1 ]] && mpc update
 
 timestart
 
@@ -21,27 +21,28 @@ echo getCoverFile( $argv[ 1 ], 'scancover' );
 EOF
 chmod +x $scandirphp
 
-function createThumbnail() {
+createThumbnail() {
 	mpdpath=${dir:9}
 	echo
 	echo ${percent}% $( tcolor "$i/$count" 8 ) $( tcolor "$mpdpath" )
-	if [[ -z $( find "$dir" -maxdepth 1 -type f ) ]]; then
-		echo "  No files found."
-		return
-	fi
-	
-	# skip if non utf-8 found
+	# skip if non utf-8 name
 	if [[ $( echo $mpdpath | grep -axv '.*' ) ]]; then
-		echo "$padR Skip - $( tcolor "$mpdpath" 1 ) contains non UTF-8 characters."
 		(( nonutf8++ ))
+		echo "$padR Skip - $( tcolor "$mpdpath" 1 ) contains non UTF-8 characters."
+		echo $mpdpath >> $nonutf8log
 		return
 	fi
 	
 	cuefile=$( find "$dir" -maxdepth 1 -type f -name '*.cue' | head -1 )
 	if [[ -z $cuefile ]]; then
 		thumbname=$( mpc ls -f "[%album%^^[%albumartist%|%artist%]]" "$mpdpath" 2> /dev/null | awk '/\^\^/ && !a[$0]++ && NF' | head -1 )
-		[[ -z $thumbname ]] && return
-		
+		if [[ -z $thumbname ]]; then
+			echo "  No files in MPD database."
+			return
+		fi
+		albumartist=$( echo $thumbname | tr -s ^^ )
+		album=$( echo $albumartist | cut -d^ -f1 )
+		artist=$( echo $albumartist | cut -d^ -f2 )
 	else
 		album=$( cat "$cuefile" | grep '^TITLE' | cut -d'"' -f2 )
 		artist=$( cat "$cuefile" | grep '^PERFORMER' | cut -d'"' -f2 )
@@ -51,13 +52,22 @@ function createThumbnail() {
 	thumbname=$( echo $thumbname | sed 's|/|\||g; s/#/{/g; s/?/}/g' )
 	thumbfile=$imgcoverarts/$thumbname.jpg
 	if (( ${#thumbfile} > 255 )); then
+		(( longname++ ))
 		echo "$padR Skip - $( tcolor "$thumbfile" 1 ) longer than 255 characters."
+		echo -e "$thumbfile\n" >> $longnamelog
 		return
 	fi
 	
-	if [[ -e "$thumbfile" && ! $removeexist ]]; then
-		(( exist++ ))
-		echo "$padW #$exist Skip - Thumbnail exists."
+	if [[ -e "$thumbfile" ]]; then
+		mpcfind=$( mpc find albumartist "$artist" album "$album" | sed 's|\(.*\)/.*|\1|' | awk '!a[$0]++' )
+		if (( $( echo "$mpcfind" | wc -l ) > 1 )); then
+			(( dup++ ))
+			echo "$padR Skip - $( tcolor "$album - $artist" 1 ) duplicate"
+			echo -e "$mpcfind\n" >> $duplog
+		elif [[ ! $removeexist ]]; then
+			(( exist++ ))
+			echo "$padW #$exist Skip - Thumbnail exists."
+		fi
 		return
 	fi
 	
@@ -73,7 +83,7 @@ function createThumbnail() {
 					echo "$padB #$prplace Replace - Existing thumbnail."
 				else
 					(( thumb++ ))
-					echo -e "$padC #$thumb New - Thumbnail created from file $cover"
+					echo -e "$padC #$thumb New - Thumbnail created from $cover"
 				fi
 				return
 			fi
@@ -102,7 +112,7 @@ function createThumbnail() {
 
 	ln -s /srv/http/assets/img/cover.svg "$dummyfile"
 	(( dummy++ ))
-	echo -e "$padW #$dummy Dummy - No coverart found."
+	echo -e "$padG #$dummy Dummy - No coverart found."
 }
 
 cue=
@@ -110,13 +120,22 @@ replace=0
 exist=0
 thumb=0
 dummy=0
-nonutf8=0
+padG=$( tcolor '.' 8 8 )
 padW=$( tcolor '.' 7 7 )
 padC=$( tcolor '.' 6 6 )
 padB=$( tcolor '.' 4 4 )
 padR=$( tcolor '.' 1 1 )
 imgcoverarts=/srv/http/assets/img/coverarts
 coverfiles='cover.jpg cover.png folder.jpg folder.png front.jpg front.png Cover.jpg Cover.png Folder.jpg Folder.png Front.jpg Front.png'
+nonutf8=0
+longname=0
+dup=0
+nonutf8log=/root/list-nonutf8.log
+longnamelog=/root/list-longnames.log
+duplog=/root/list-duplicates.log
+echo -e "Non-UTF8 Named Files - $( date +"%D %T" )\n" > $nonutf8log
+echo -e "Too Long Named Files - $( date +"%D %T" )\n" > $longnamelog
+echo -e "Duplicate Artist-Album - $( date +"%D %T" )\n" > $duplog
 
 [[ -n $( ls $imgcoverarts ) ]] && update=Update || update=Create
 coloredname=$( tcolor 'Browse By CoverArt' )
@@ -138,10 +157,26 @@ done
 chown -h http:http $imgcoverarts/*
 
 echo -e               "\n\n$padC New thumbnails       : $( tcolor $( numfmt --g $thumb ) )"
-(( $replace )) && echo -e "$padB Replaced thumbnails  : $( tcolor $( numfmt --g $replace ) )"
-(( $exist )) && echo -e   "$padW Existings thumbnails : $( tcolor $( numfmt --g $exist ) )"
-(( $dummy )) && echo -e   "$padW Dummy thumbnails     : $( tcolor $( numfmt --g $dummy ) )"
-(( $nonutf8 )) && echo -e "$padR Non UTF-8 path       : $( tcolor $( numfmt --g $(nonutf8) ) )"
+(( $replace )) && echo -e "$padB Replaced thumbnails  : $( tcolor $( numfmt --g $replace ) 4 )"
+(( $exist )) && echo -e   "$padW Existings thumbnails : $( numfmt --g $exist )"
+(( $dummy )) && echo -e   "$padG Dummy thumbnails     : $( tcolor $( numfmt --g $dummy ) 8 )"
+if (( $nonutf8 )); then
+	echo -e               "$padR Non UTF-8 path       : $( tcolor $( numfmt --g $nonutf8 ) 1 )  (See list in $( tcolor "$nonutf8log" ))"
+else
+	rm $nonutf8log
+fi
+if (( $longname )); then
+	echo -e              "$padR Too long named       : $( tcolor $( numfmt --g $longname ) 1 )  (See list in $( tcolor "$longnamelog" ))"
+else
+	rm $longnamelog
+fi
+if (( $dup )); then
+	echo "$( awk '!NF || !seen[$0]++' $duplog | cat -s )" > $duplog # remove duplicate files
+	dup=$(( $( grep -cve '^\s*$' $duplog ) - 1 )) # count without blank lines and less header
+	echo -e              "$padR Duplicate albums     : $( tcolor $( numfmt --g $dup ) 1 )  (See list in $( tcolor "$duplog" ))"
+else
+	rm $duplog
+fi
 echo
 echo -e                       "      Total thumbnails : $( tcolor $( numfmt --g $( ls -1 $imgcoverarts | wc -l ) ) )"
 echo -e  "      Parsed directory : $( tcolor "$path" )"
