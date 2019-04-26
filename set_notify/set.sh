92c52636813f3af1b21c99617baea0e95d4bd6e3
rern <rernrern@gmail.com>
2019-04-26 11:03:26 +0700

    Update set.sh

--- set_notify/set.sh
+++ set_notify/set.sh
@@ -10,7 +10,7 @@ file=/srv/http/assets/js/enhancebanner.js
 if [[ -e $file ]]; then
 	sed -i "s/.000/${1}000/" $file
 else
-	sed -i "s/[0-9]\+\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
+	sed -i "s/[0-9]\+\( : notify.delay\)/${1}000 ))\1/" /srv/http/assets/js/runeui.js
 	sed -i "s/[0-9]\+e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
 fi
 redis-cli hset settings notify $1 &> /dev/null
