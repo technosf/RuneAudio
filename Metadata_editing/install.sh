e4a64428b5a7d4412fab47981be42d83f2cf3e6d
rern <rernrern@gmail.com>
2019-05-08 14:00:45 +0700

    Update install.sh

--- Metadata_editing/install.sh
+++ Metadata_editing/install.sh
@@ -12,16 +12,19 @@ fi
 title -l '=' "$bar Install $( tcolor Kid3 ) ..."
 timestart l
 
-echo -e "$bar Get files ..."
+if [[ ! -e /lib/libicudata.so.64.2 ]]; then
+	echo -e "$bar Get files ..."
 
-wgetnc https://github.com/rern/RuneAudio/raw/master/Metadata_editing/kid3lib.tar.xz
-cp -f /usr/lib/libcrypto.so.1.1{,X}
-cp -f /usr/lib/libssl.so.1.1{,X}
-bsdtar xvf kid3lib.tar.xz -C /usr/lib
-rm kid3lib.tar.xz
-ln -sf /usr/lib/libreadline.so.{8.0,8}
+	wgetnc https://github.com/rern/RuneAudio/raw/master/Metadata_editing/kid3lib.tar.xz
+	cp -f /usr/lib/libcrypto.so.1.1{,X}
+	cp -f /usr/lib/libssl.so.1.1{,X}
+	bsdtar xvf kid3lib.tar.xz -C /usr/lib
+	rm kid3lib.tar.xz
+	ln -sf /usr/lib/libreadline.so.{8.0,8}
+fi
 pacman -Sy --noconfirm glibc
 [[ $( redis-cli get release ) == 0.4b ]] && pacman -S --noconfirm pcre2 harfbuzz freetype2
+pacman -R --noconfirm libsystemd
 pacman -S --noconfirm kid3
 
 timestop l
