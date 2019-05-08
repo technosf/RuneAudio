939dac164d871cb5977557b7d2b1b886d7301a22
rern <rernrern@gmail.com>
2019-05-08 18:09:24 +0700

    Update install.sh

--- Metadata_editing/install.sh
+++ Metadata_editing/install.sh
@@ -24,7 +24,7 @@ if [[ ! -e /lib/libicudata.so.64.2 ]]; then
 fi
 pacman -Sy --noconfirm glibc
 [[ $( redis-cli get release ) == 0.4b ]] && pacman -S --noconfirm pcre2 harfbuzz freetype2
-pacman -Rdd libsystend
+pacman -Rdd --noconfirm libsystend
 pacman -S --noconfirm kid3
 
 timestop l
