12ca76777463dd2496aa560e44789101e2d5a7a3
rern <rernrern@gmail.com>
2019-05-13 08:55:11 +0700

    Update set.sh

--- set_wlan-bt/set.sh
+++ set_wlan-bt/set.sh
@@ -4,11 +4,11 @@ rm $0
 
 . /srv/http/addonstitle.sh
 
-if [[ $1 == 'yes' ]]; then
+if [[ $1 == yes ]]; then
 	title -l '=' "$bar Disable WiFi and Bluetooth ..."
 
 	systemctl disable netctl-auto@wlan0
-	systemctl stop netctl-auto@wlan0 shairport upmpdcli
+	systemctl stop netctl-auto@wlan0
 	rfkill block 0
 	sed -i '/blacklist/ s/^#//' /etc/modprobe.d/disable_rpi3_wifi_bt.conf
 
@@ -18,7 +18,7 @@ else
 
 	sed -i '/blacklist/ s/^/#/' /etc/modprobe.d/disable_rpi3_wifi_bt.conf
 	systemctl enable netctl-auto@wlan0
-	systemctl start netctl-auto@wlan0 shairport upmpdcli
+	systemctl start netctl-auto@wlan0
 
 	title -nt "$info WiFi and Bluetooth enabled."
 fi
