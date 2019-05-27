fa76913fa0f30ae8f8707a18d35f03ec2ecb4f16
rern <rernrern@gmail.com>
2019-05-27 21:27:02 +0700

    Update install.sh

--- kid3-cli/install.sh
+++ kid3-cli/install.sh
@@ -9,6 +9,24 @@ if pacman -Q kid3-cli &> /dev/null; then
 fi
 
 title -l '=' "$bar Install $( tcolor kid3-cli ) ..."
+timestart l
+
+if [[ ! -e /lib/libicudata.so.64.2 ]]; then
+	echo -e "$bar Get files ..."
+
+	wgetnc https://github.com/rern/RuneAudio/raw/master/Metadata_editing/kid3lib.tar.xz
+	cp /usr/lib/libcrypto.so.1.1{,X} &> /dev/null
+	cp /usr/lib/libssl.so.1.1{,X} &> /dev/null
+	bsdtar xvf kid3lib.tar.xz -C /usr/lib
+	rm kid3lib.tar.xz
+	ln -sf /usr/lib/libreadline.so.{8.0,8
+	ln -f /usr/lib/libicudata.so.64{.2,}
+	ln -f /usr/lib/libicui18n.so.64{.2,}
+	ln -f /usr/lib/libicuio.so.64{.2,}
+	ln -f /usr/lib/libicuuc.so.64{.2,}
+fi
+
+pacman -S glibc
 
 file=kid3-cli-3.7.1-1-armv7h.pkg.tar.xz
 wget https://github.com/rern/RuneAudio/raw/master/kid3-cli/$file
