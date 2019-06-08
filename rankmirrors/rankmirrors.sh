946ba75f088a6cc49db7e7a183ce1d7cbf9719ea
rern <rernrern@gmail.com>
2019-06-08 21:16:14 +0700

    Add files via upload

--- rankmirrors/rankmirrors.sh
+++ rankmirrors/rankmirrors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm $0
+#rm $0
 
 (( $# == 0 )) && sec=5 || sec=$1
 
@@ -19,63 +19,63 @@ wgetnc https://github.com/archlinuxarm/PKGBUILDs/raw/master/core/pacman-mirrorli
 tmplist=/tmp/mirrorlist
 echo $( grep 'Generated' $tmplist | cut -d' ' -f2- )
 
-dlfile='armv7h/community/community.db' # download test file
 # convert mirrorlist to url list
 if grep -qs '# Server = ' $tmplist; then
 	sed -i '/^\s*$/d
 		/^# Server = /!d
 		s/^# Server = //g
-		s|$arch/$repo|'$dlfile'|g' $tmplist
+		s|/$arch/$repo||g' $tmplist
 		# delete blank lines and lines not start with '# Server = ', remove '# Server = '
 else
 	sed -i 's/^Server = //g
-		s|$arch/$repo|'$dlfile'|g' $tmplist # already uncomment
+		s|/$arch/$repo||g' $tmplist # already uncomment
 fi
 
-IFS=$'\n' read -d '' -r -a servers < $tmplist # convert list to array
+readarray servers < "$tmplist"
 
-title -l = $bar Rank mirror servers by download speed ...
+title -l = $bar Rank package servers ...
 echo
-echo Test ${#servers[@]} servers @ $sec seconds:
+echo Test ${#servers[@]} servers @ $sec seconds download + 3 pings:
 echo
 
-tmpdir=/tmp/rankmirrors/
+dlfile='armv7h/community/community.db' # download test file
+tmpdir=/tmp/rankmirrors
 rm -rf $tmpdir && mkdir $tmpdir
-i=0
-dl_server=''
-for server in ${servers[*]}; do # download from each mirror
-	timeout $sec wget -q -P $tmpdir $server &
-	wait
-	dl=$(du -c $tmpdir | grep total | awk '{print $1}') # get downloaded amount
 
-	server0='Server = '${servers[$i]/armv7h\/community\/community.db/\$arch\/\$repo} # for ranked mirrorlist file
-	dl_server="$dl_server$dl $server0\n" # with download amount for ranking
+i=0
+for server in ${servers[@]}; do # download from each mirror
+	(( i++ ))
+	timeout $sec wget -q -P $tmpdir $server/$dlfile &
+	wait
+	dl=$( du -c $tmpdir | grep total | awk '{print $1}' ) # get downloaded amount
+	ping=$( ping -c 3 ${server/http*\:\/\/} | tail -1 | cut -d'/' -f5 )
+	[[ -z $ping ]] && ping=
 	
-	shorturl=$( sed 's/archlinux.*/../' <<< ${servers[$i]} ) # short url for output
-	speed=$(expr $dl / $sec)
-	((i+=1))
-	printf "%3d. %-25s : %5d kB/s\n" $i $shorturl $speed
+	server0='Server = '$server'/$arch/$repo' # for ranked mirrorlist file
+	speed=$(( dl / sec ))
+	dl_server="$dl_server$server0 $dl $ping\n" # with download amount for ranking
+	printf "%3d. %-24s : %5d kB/s %9s ms\n" $i ${server/archlinux*}.. $speed $ping
 	
-	rm $tmpdir* &>/dev/null & # remove downloaded file
+	rm -f $tmpdir/* # remove downloaded file
 done
 
-rank=$( echo -e "$dl_server" | sort -k1 -n -r ) # sort by '1st column' 'as number' 'reverse order'
-rankfile=$( echo -e "$rank" | cut -f2-4 -d' ' ) # keep '2nd-4th column' 'devided by space' (remove 1st column)
+rank=$( echo -e "$dl_server" | grep . | sort -g -k4,4nr -k5n )
+rankfile=$( echo -e "$rank" | cut -d' ' -f1-3 ) # keep '1st-3rd column' 'devided by space' (remove 1st column)
 
 echo
-echo -e "$info Mirror list was updated with these servers top the list:"
+echo -e "$info Top 3 package servers ranked by speed and latency:"
 echo
-echo -e "$rankfile" | sed -n 1,3p
-
+echo -e "$rankfile" | head -3
+echo
 list=/etc/pacman.d/mirrorlist
 [[ ! -e $list.backup ]] && cp $list $list.backup
 echo -e "$rankfile" > $list
 rm -rf $tmpdir
 
 echo -e "$bar Update package database ..."
-killall -q pacman
+
 rm -f /var/lib/pacman/db.lck
 pacman -Sy
 
 timestop
-title -l = "$bar Mirror list ranked and updated successfully."
+title -l = "$bar Mirror list updated and ranked successfully."
