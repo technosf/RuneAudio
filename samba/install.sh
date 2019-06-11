071434a06e53b5e3da264835deb9478fa21ef35e
rern <rernrern@gmail.com>
2019-06-11 15:47:11 +0700

    Update install.sh

--- samba/install.sh
+++ samba/install.sh
@@ -30,23 +30,12 @@ if [[ ! $( pacman -Qs samba4-rune ) ]]; then
 	exit
 fi
 
-if [[ ! -e /usr/bin/kid3-cli ]]; then
-	file=libcryptossl.tar.xz
-	wgetnc https://github.com/rern/_assets/raw/master/$file
-	cp /usr/lib/libcrypto.so.1.1{,backup} &> /dev/null
-	cp /usr/lib/libssl.so.1.1{,backup} &> /dev/null
-	bsdtar xvf $file -C /usr/lib
-	rm $file
-fi
-
 systemctl stop nmbd smbd
 mv /etc/samba/smb-prod.conf{,.backup}
 
 pacman -R --noconfirm samba4-rune
 
 pacman -S --noconfirm --force libnsl
-glibc=$( pacman -Ss 'glibc' | head -1 | cut -d' ' -f4 )
-[[ $glibc != '[installed]' ]] && pacman -S --noconfirm glibc
 pacman -S --noconfirm ldb libtirpc tdb tevent python smbclient samba
 pacman -S --noconfirm libwbclient
 
