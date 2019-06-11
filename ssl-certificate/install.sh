ae752fcb09b48f41a69c8b848dc3ae4bb7d2a05a
rern <rernrern@gmail.com>
2019-06-11 16:13:52 +0700

    Update install.sh

--- ssl-certificate/install.sh
+++ ssl-certificate/install.sh
@@ -8,11 +8,10 @@ timestart
 
 title -l '=' "$bar Install SSL Certicicate ..."
 
-pacman -Sy --noconfirm certbot certbot-nginx python-setuptools python-six python-appdirs python-pyparsing
-wgetnc https://github.com/rern/RuneAudio/raw/master/mpd/usr/lib/libcrypto.so.1.1 -P /usr/lib
-wgetnc https://github.com/rern/RuneAudio/raw/master/mpd/usr/lib/libssl.so.1.1 -P /usr/lib
-chown root:root /usr/lib/{libcrypto.so.1.1,libssl.so.1.1}
-chmod 755 /usr/lib/{libcrypto.so.1.1,libssl.so.1.1}
+rankmirrors
+
+pacman -S --needed --noconfirm certbot certbot-nginx python-setuptools python-six python-appdirs python-pyparsing
+
 certbot --nginx
 
 timestop
