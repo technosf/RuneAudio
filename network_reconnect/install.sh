72158fd601a6cf55867878de2eadbbbbf1939ef7
rern <rernrern@gmail.com>
2019-06-11 16:03:17 +0700

    Delete install.sh

--- network_reconnect/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. /srv/http/addonstitle.sh
-
-title -l '=' "$bar Upgrade $( tcolor "ifplugd" ) ..."
-
-pacman -Sy --noconfirm ifplugd
-
-title -l '=' "$bar $( tcolor "ifplugd" ) upgraded successfully."
