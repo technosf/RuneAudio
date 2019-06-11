0bd171af64061f0a4ebf291d7e02eaed5b12351a
rern <rernrern@gmail.com>
2019-06-11 15:50:54 +0700

    Update install.sh

--- mpd/install.sh
+++ mpd/install.sh
@@ -39,10 +39,10 @@ if [[ ! -e /usr/lib/libicudata.so.64.2 ]]; then
 	bsdtar xvf kid3lib.tar.xz -C /usr/lib
 	rm kid3lib.tar.xz
 	ln -sf /usr/lib/libreadline.so.8{.0,}
-	ln -f /usr/lib/libicudata.so.64{.2,}
-	ln -f /usr/lib/libicui18n.so.64{.2,}
-	ln -f /usr/lib/libicuio.so.64{.2,}
-	ln -f /usr/lib/libicuuc.so.64{.2,}
+	ln -sf /usr/lib/libicudata.so.64{.2,}
+	ln -sf /usr/lib/libicui18n.so.64{.2,}
+	ln -sf /usr/lib/libicuio.so.64{.2,}
+	ln -sf /usr/lib/libicuuc.so.64{.2,}
 fi
 
 echo -e "$bar Remove conflict packages ..."
