429ef28804f0f94fb0a38faea544f4da44708ed2
rern <rernrern@gmail.com>
2019-06-28 18:17:32 +0700

    Add files via upload

--- expand_partition/systemd/expand.sh
+++ expand_partition/systemd/expand.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# 'partprope', 'libparted.so.2' needed
+rm $0
 
 unpartb=$( sfdisk -F | grep /dev/mmcblk0 | awk '{print $6}' )
-# noobs has 3MB unpartitioned space > skip
-if (($unpartb > 10000000)); then
-	echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
-	partprobe /dev/mmcblk0
-	resize2fs /dev/mmcblk0p2
-fi
+
+echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
+partprobe /dev/mmcblk0
+resize2fs /dev/mmcblk0p2
 
 systemctl disable expand
-rm /etc/systemd/system/rootexpand.service
+rm /etc/systemd/system/expand.service
 systemctl daemon-reload
-rm /root/expand.sh
