8ceb01ac0379020e9912c396d645fa3fc7d931a9
rern <rernrern@gmail.com>
2019-06-29 19:08:12 +0700

    Create setup.sh

---
+++ RuneAudio+RuneUIe.img/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. /srv/http/addonstitle.sh
+
+title -l '=' "$bar Setup SD card for image file ..."
+
+# remove special directories
+rm -r /srv/http/assets/img/{bookmarks,coverarts,lyrics,playlists,tmp,webradiopl,webradios}
+
+# clear packages cache
+rm /var/cache/pacman/pkg/*
+
+# mpd database reset
+rm /var/lib/mpd/mpd.db /var/lib/mpd/playlists/*
+umount /dev/sda1
+mpc update
+
+# mirrorlist reset
+wget https://github.com/archlinuxarm/PKGBUILDs/raw/master/core/pacman-mirrorlist/mirrorlist -P /etc/pacman.d
+
+# run once script
+wget https://github.com/rern/RuneAudio/raw/master/RuneAudio%2BRuneUIe.img/systemd/runonce.sh -P /root
+chmod +x /root/runonce.sh
+
+# systemd unit file
+wget https://github.com/rern/RuneAudio/raw/master/RuneAudio%2BRuneUIe.img/runonce.service -P /etc/systemd/system
+systemctl enable runonce
+
+echo -e "$bar SD card ready for read to image file."
