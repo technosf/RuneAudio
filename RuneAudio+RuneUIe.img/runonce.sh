7ee197d2428bbda2bd34c2dccceb7d5dec606e3c
rern <rernrern@gmail.com>
2019-06-29 12:28:30 +0700

    Update runonce.sh

--- RuneAudio+RuneUIe.img/runonce.sh
+++ RuneAudio+RuneUIe.img/runonce.sh
@@ -7,6 +7,9 @@ echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
 partprobe /dev/mmcblk0
 resize2fs /dev/mmcblk0p2
 
+# wait for usb/nas drive mounted
+sleep 10
+
 # makeDirLink
 . /srv/http/addonstitle.sh
 makeDirLink bookmarks
