d08218fa7250bea3b36cb6d1026e315e638cb08e
rern <rernrern@gmail.com>
2019-07-01 06:56:43 +0700

    Update mountimg.sh

--- RuneAudio+RuneUIe.img/mountimg.sh
+++ RuneAudio+RuneUIe.img/mountimg.sh
@@ -10,10 +10,10 @@ unitbyte=$( echo "$fd" | grep '^Units' | cut -d' ' -f8 )
 
 if [[ $# == 1 ]]; then  # root
   start=$( echo "$fd" | tail -1 )
-  $mntpoint=/media/root
+  mntpoint=/media/root
 else                    # boot
   start=$( echo "$fd" | tail -2 | head -1 )
-  $mntpoint=/media/boot
+  mntpoint=/media/boot
 fi
 mkdir -p $path
 mount -o loop,offset=$(( unitbyte * start )) "$img" $mntpoint
