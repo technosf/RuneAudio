93cb3723e74561f7621d17cdfe5e2b94a3f409fa
rern <rernrern@gmail.com>
2019-07-01 06:49:56 +0700

    Create mountimg.sh

---
+++ RuneAudio+RuneUIe.img/mountimg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# usage: mountimg FILE.img [1]
+1 - /boot partition
+
+img=$1
+
+fd=$( fdisk -lo Start "$img" )
+unitbyte=$( echo "$fd" | grep '^Units' | cut -d' ' -f8 )
+
+if [[ $# == 1 ]]; then  # root
+  start=$( echo "$fd" | tail -1 )
+  $mntpoint=/media/root
+else                    # boot
+  start=$( echo "$fd" | tail -2 | head -1 )
+  $mntpoint=/media/boot
+fi
+mkdir -p $path
+mount -o loop,offset=$(( unitbyte * start )) "$img" $mntpoint
+echo "Partition available at $mntpoint
