10e16d3cfd0b048986671d8a39b2b3d9fb0031e2
rern <rernrern@gmail.com>
2019-07-03 11:09:14 +0700

    Create install.sh

---
+++ extradir/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. /srv/http/addonstitle.sh
+
+title -l '=' "$bar Restore extra directories ..."
+
+makeLink() {
+	direxist=$( find /mnt/MPD/ -maxdepth 3 -type d -name $1 )
+	if [[ -e "$direxist" ]]; then
+		ln -sf "$direxist" /srv/http/assets/img/
+		echo $dir
+	else
+		echo -e "$warn $1 not found."
+	fi
+}
+
+for dir in bookmarks coverarts lyrics playlists tmp webradiopl webradios; do
+	makeLink $dir
+done
+
+title "$bar Extra directories restored successfully."
