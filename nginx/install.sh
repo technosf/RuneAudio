1e7bd6cb6459794d25d5b5caccdacb707620f360
rern <rernrern@gmail.com>
2019-07-03 17:08:56 +0700

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# change version number in RuneAudio_Addons/srv/http/addonslist.php
-
-alias=ngin
-
 . /srv/http/addonstitle.sh
 
 if grep -q 'rewrite /css/(.*)   ' /etc/nginx/nginx.conf; then
@@ -15,9 +11,6 @@ fi
 title -l '=' "$bar Upgrade NGINX ..."
 timestart
 
-# backup
-mv /etc/nginx/html/50x.html{.backup,}
-
 echo -e "$bar Get NGINX packages ..."
 
 file=nginx-1.16.0-1-armv7h.pkg.tar.xz
@@ -28,12 +21,9 @@ echo -e "y \n" | pacman -U $file
 pacman -Sy iptables
 
 rm $file
-mv /etc/nginx/html/50x.html{,.backup}
 wgetnc https://github.com/rern/RuneAudio/raw/master/nginx/nginx.conf -P /etc/nginx
 wgetnc https://github.com/rern/RuneAudio/raw/master/nginx/50x.html -P /srv/http
 chown http:http /srv/http/50x.html
-
-redis-cli hset addons ngin 1 &> /dev/null # mark as upgraded - disable button
 
 echo -e "$bar Restart NGINX ..."
 
