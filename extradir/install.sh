69b20c78fc69c5558e23ac7afba71deb114ca3d4
rern <rernrern@gmail.com>
2019-07-06 11:57:19 +0700

    Update install.sh

--- extradir/install.sh
+++ extradir/install.sh
@@ -7,7 +7,8 @@ title -l '=' "$bar Restore extra directories ..."
 makeLink() {
 	direxist=$( find /mnt/MPD/ -maxdepth 3 -type d -name $1 )
 	if [[ -e "$direxist" ]]; then
-		ln -sf "$direxist" /srv/http/assets/img/
+		rm -r /srv/http/assets/img/$1
+		ln -s "$direxist" /srv/http/assets/img/
 		echo $dir
 	else
 		echo -e "$warn $1 not found."
