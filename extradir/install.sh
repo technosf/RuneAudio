3d50136356b0d7f45fd1c583eb4883135a8607c3
rern <rernrern@gmail.com>
2019-07-08 13:28:42 +0700

    Update install.sh

--- extradir/install.sh
+++ extradir/install.sh
@@ -6,14 +6,14 @@ title -l '=' "$bar Restore extra directories ..."
 
 for dir in bookmarks coverarts lyrics playlists tmp webradiopl webradios; do
 	direxist=$( find /mnt/MPD/ -maxdepth 3 -type d -name $dir )
-	if [[ -e "$direxist" ]]; then
-		rm -r /srv/http/assets/img/$dir
-		ln -s "$direxist" /srv/http/assets/img/
-		echo $dir
-	else
+	if [[ ! -e "$direxist" ]]; then
 		echo -e "$info Extra directories not found."
 		exit
 	fi
+	
+	rm -r /srv/http/assets/img/$dir
+	ln -s "$direxist" /srv/http/assets/img/
+	echo $dir
 done
 
 title "$bar Extra directories restored successfully."
