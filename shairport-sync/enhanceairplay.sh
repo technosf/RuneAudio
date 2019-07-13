bc76ce66dfba1d42481eb729b02622970da5d5b7
rern <rernrern@gmail.com>
2019-07-13 17:38:24 +0700

    Add files via upload

---
+++ shairport-sync/enhanceairplay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $1 == off ]]; then
+	systemctl start mpd
+	redis-cli set activePlayer MPD
+	curl -s -X POST 'http://localhost/pub?id=airplay' -d 0
+else
+	systemctl stop mpd
+	redis-cli set activePlayer Airplay
+	curl -s -X POST 'http://localhost/pub?id=airplay' -d 1
+fi
