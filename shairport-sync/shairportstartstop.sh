7159e1e184f135b22a9f4ac70b049cd912843316
rern <rernrern@gmail.com>
2019-07-14 10:40:23 +0700

    Add files via upload

--- shairport-sync/shairportstartstop.sh
+++ shairport-sync/shairportstartstop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+echo $#
 if (( $# > 0 )); then
-	systemctl start mpd
-	redis-cli set activePlayer MPD
+	sudo systemctl start mpd
+	sudo redis-cli set activePlayer MPD
 	curl -s -X POST 'http://localhost/pub?id=airplay' -d 0
 else
-	systemctl stop mpd
-	redis-cli set activePlayer Airplay
+	sudo systemctl stop mpd
+	sudo redis-cli set activePlayer Airplay
 	curl -s -X POST 'http://localhost/pub?id=airplay' -d 1
 fi
