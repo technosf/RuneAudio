6c28de9ad2f086e6ac50a6ca55a7f0f5ed9b8ccc
rern <rernrern@gmail.com>
2019-07-14 09:26:08 +0700

    Add files via upload

--- RuneAudio+RuneUIe.img/settings.php
+++ RuneAudio+RuneUIe.img/settings.php
@@ -38,40 +38,6 @@
                     <span class="help-block">Set the system timezone</span>
                 </div>
             </div>
-            <!-- <div <?php if($this->proxy['enable'] === 1): ?>class="boxed-group"<?php endif ?> id="proxyBox">
-                <div class="form-group">
-                    <label for="proxy" class="control-label col-sm-2">HTTP Proxy server</label>
-                    <div class="col-sm-10">
-                        <label class="switch-light well" onclick="">
-                            <input id="proxy" name="features[proxy]" type="checkbox" value="1"<?php if($this->proxy['enable'] == 1): ?> checked="checked" <?php endif ?>>
-                            <span><span>OFF</span><span>ON</span></span><a class="btn btn-primary"></a>
-                        </label>
-                    </div>
-                </div>
-                <div class="<?php if($this->proxy['enable'] != 1): ?>hide<?php endif ?>" id="proxyAuth">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="proxy-user">Host</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="text" id="proxy_host" name="features[proxy][host]" value="<?php echo $this->proxy['host']; ?>" data-trigger="change" placeholder="<host IP or FQDN>:<port>">
-                            <span class="help-block">Insert HTTP Proxy host<i> (format: proxy_address:port)</i></span>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="proxy-user">Username</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="text" id="proxy_user" name="features[proxy][user]" value="<?php echo $this->proxy['user']; ?>" data-trigger="change" placeholder="user">
-                            <span class="help-block">Insert your HTTP Proxy <i>username</i> (leave blank for anonymous authentication)</span>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="proxy-pass">Password</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="password" id="proxy_pass" name="features[proxy][pass]" value="<?php echo $this->proxy['pass']; ?>" placeholder="pass" autocomplete="off">
-                            <span class="help-block">Insert your HTTP Proxy <i>password</i> (case sensitive) (leave blank for anonymous authentication)</span>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <div class="form-group form-actions">
                 <div class="col-sm-offset-2 col-sm-10">
                     <button class="btn btn-primary btn-lg" value="save" name="save" type="submit">Apply settings</button>
@@ -92,19 +58,6 @@
                     <span class="help-block">No other kernels available.</span>
                 </div>
             </div>
-            <!--
-            <div class="form-group">
-                <label class="control-label col-sm-2" for="i2smodule">Linux Kernel</label>
-                <div class="col-sm-10">
-                    <select class="selectpicker" name="kernel" data-style="btn-default btn-lg">
-                        <option value="linux-arch-rpi_3.12.26-1-ARCH" <?php if($this->kernel === 'linux-arch-rpi_3.12.26-1-ARCH'): ?> selected <?php endif ?>>Linux kernel 3.12.26-1&nbsp;&nbsp;&nbsp;ARCH&nbsp;[RuneAudio v0.3-beta]</option>
-                        <option value="linux-rune-rpi_3.12.19-2-ARCH" <?php if($this->kernel === 'linux-rune-rpi_3.12.19-2-ARCH'): ?> selected <?php endif ?>>Linux kernel 3.12.19-2&nbsp;&nbsp;&nbsp;RUNE&nbsp;[RuneAudio v0.3-alpha]</option>
-                        <option value="linux-rune-rpi_3.6.11-18-ARCH+" <?php if($this->kernel === 'linux-rune-rpi_3.6.11-18-ARCH+'): ?> selected <?php endif ?>>Linux kernel 3.6.11-18&nbsp;&nbsp;&nbsp;ARCH+&nbsp;[RuneAudio v0.1-beta/v0.2-beta]</option>
-                        <option value="linux-rune-rpi_3.12.13-rt21_wosa" <?php if($this->kernel === 'linux-rune-rpi_3.12.13-rt21_wosa'): ?> selected <?php endif ?>>Linux kernel 3.12.13-rt&nbsp;&nbsp;&nbsp;RUNE-RT&nbsp;[Wolfson Audio Card]</option>
-                    </select>
-                    <span class="help-block">Switch Linux Kernel version (REBOOT REQUIRED). <strong>Linux kernel 3.12.26-1</strong> is the default kernel in the current release, <strong>Linux kernel 3.12.19-2</strong> is the kernel used in RuneAudio v0.3-alpha, <strong>Linux kernel 3.6.11-18</strong> is the kernel used in RuneAudio v0.1-beta/v0.2-beta (it has no support for I&#178;S), <strong>Linux kernel 3.12.13-rt</strong> is an EXPERIMENTAL kernel (not suitable for all configurations), it is optimized for <strong>Wolfson Audio Card</strong> support and it is the default option for that type of soundcard</span>
-                </div>
-            -->
             <div class="form-group">
                 <label class="control-label col-sm-2" for="i2smodule_select">I&#178;S kernel modules</label>
                 <div class="col-sm-10">
@@ -205,23 +158,6 @@
                 </div>
             </div>
             <?php endif;?>
-            <!-- <div 
-            <?php if($this->hwplatformid === '09'): ?>
-            <div class="form-group">
-                <label class="control-label col-sm-2" for="i2smodule">Linux Kernel</label>
-                <div class="col-sm-10">
-                    <select class="selectpicker" name="kernel" data-style="btn-default btn-lg">
-                        <option value="linux-ARCH"><?php echo $this->kernel; ?></option>
-                    </select>
-                    <span class="help-block">There are no other kernels available at the moment!</span>
-                </div>
-                <label class="control-label col-sm-2" for="i2smodule">I&#178;S kernel modules</label>
-                <div class="col-sm-10">
-                    <span class="help-block">Enable I&#178;S output by editing /boot/boot.ini. Once set, the output interface will appear in the <a href="/mpd/">MPD configuration select menu</a>, and modules will also auto-load from the next reboot.</span>
-                </div>
-            </div>
-            <?php endif;?>
-            </div> -->
             <?php if($this->hwplatformid === '10'): ?>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="i2smodule">Linux Kernel</label>
@@ -273,7 +209,6 @@
         <fieldset id="features-management">
             <legend>Features management</legend>
             <p>Enable/disable optional modules that best suit your needs. Disabling unused features will free system resources and might improve the overall performance</p>
-<?php /*xxx            <div <?php if($this->airplay['enable'] === '1'): ?>class="boxed-group"<?php endif ?> id="airplayBox">xxx*/ ?>
                 <div class="form-group">
                     <label for="airplay" class="control-label col-sm-2">AirPlay</label>
                     <div class="col-sm-10">
@@ -284,46 +219,6 @@
                         <span class="help-block">Toggle the capability of receiving wireless streaming of audio via AirPlay protocol</span>
                     </div>
                 </div>
-<?php /*xxx                <div class="<?php if($this->airplay['enable'] != 1): ?>hide<?php endif ?>" id="airplayName">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="airplay-name">AirPlay name</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="text" id="airplay_name" name="features[airplay][name]" value="<?php echo $this->airplay['name']; ?>" data-trigger="change" placeholder="runeaudio">
-                            <span class="help-block">AirPlay broadcast name</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div <?php if($this->spotify['enable'] === '1'): ?>class="boxed-group"<?php endif ?> id="spotifyBox">
-                <div class="form-group">
-                    <label for="spotify" class="control-label col-sm-2">Spotify</label>
-                    <div class="col-sm-10">
-                        <label class="switch-light well" onclick="">
-                            <input id="spotify" name="features[spotify][enable]" type="checkbox" value="1"<?php if($this->spotify['enable'] === '1'): ?> checked="checked" <?php endif ?> <?php if($this->activePlayer === 'Spotify'): ?>disabled readonly<?php endif; ?>>
-                            <?php if($this->activePlayer === 'Spotify'): ?><input id="spotify" name="features[spotify][enable]" type="hidden" value="1"><?php endif; ?>
-                            <span><span>OFF</span><span>ON</span></span><a class="btn btn-primary <?php if($this->activePlayer === 'Spotify'): ?>disabled<?php endif; ?>"></a>
-                        </label>
-                        <span class="help-block">Due to the Spotify upgrade of May 2018 the Spotify client no longer works<br>
-						<i>Enable Spotify client. You must have a <strong><a href="https://www.spotify.com/premium/" target="_blank">Spotify PREMIUM</a></strong> account</i></span>
-                    </div>
-                </div>
-                <div class="<?php if($this->spotify['enable'] != 1): ?>hide<?php endif ?>" id="spotifyAuth">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="spotify-usr">Username</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="text" id="spotify_user" name="features[spotify][user]" value="<?php echo $this->spotify['user']; ?>" data-trigger="change" placeholder="user" autocomplete="off">
-                            <span class="help-block">Insert your Spotify <i>username</i></span>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="spotify-pasw">Password</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="password" id="spotify_pass" name="features[spotify][pass]" value="<?php echo $this->spotify['pass']; ?>" placeholder="pass" autocomplete="off">
-                            <span class="help-block">Insert your Spotify <i>password</i> (case sensitive)</span>
-                        </div>
-                    </div>
-                </div>
-            </div>xxx*/ ?>
             <div <?php if($this->dlna['enable'] === '1'): ?>class="boxed-group"<?php endif ?> id="dlnaBox">
                 <div class="form-group">
                     <label for="dlna" class="control-label col-sm-2">UPnP / DLNA</label>
@@ -338,11 +233,6 @@
                 </div>
                 <div class="<?php if($this->dlna['enable'] != 1): ?>hide<?php endif ?>" id="dlnaName">
                     <div class="form-group">
-<?php /*xxx                        <label class="control-label col-sm-2" for="dlna-name">UPnP / DLNA name</label>
-                        <div class="col-sm-10">
-                            <input class="form-control osk-trigger input-lg" type="text" id="dlna_name" name="features[dlna][name]" value="<?php echo $this->dlna['name']; ?>" data-trigger="change" placeholder="runeaudio">
-                            <span class="help-block">UPnP / DLNA broadcast name</span>
-                        </div>xxx*/ ?>
                         <label class="control-label col-sm-2" for="dlna-queueowner">UPnP / DLNA is MPD queue owner</label>
                         <div class="col-sm-10">
 							<label class="switch-light well" onclick="">
@@ -398,13 +288,6 @@
                             <span class="help-block">Switch this ON if you use a mouse with your local browser display, this is not normally used with a touchscreen</span>
                         </div>
                     </div>
-<?php /*enha					<div class="form-group">
-						<label class="col-sm-2 control-label" for="localSStime">Local ScreenSaver time</label>
-						<div class="col-sm-10">
-							<input class="form-control osk-trigger input-lg" type="number" id="localSStime" name="features[localSStime]" value="<?=$this->localSStime ?>" data-trigger="change" min="-1" max="100" placeholder="-1" />
-							<span class="help-block">Sets the activation time for the local browser screensaver (0-100 seconds, -1 disables the feature)</span>
-						</div>
-					</div>enha*/ ?>
 				</div>
 				<?php else: ?>
 				<div class="form-group">
@@ -425,13 +308,6 @@
                     <span class="help-block">Protect the UI with a password (standard is "rune" can be changed on login screen)</span>
                 </div>
             </div>
-<?php /*enha            <div class="form-group">
-                <label class="col-sm-2 control-label" for="remoteSStime">Remote ScreenSaver time</label>
-                <div class="col-sm-10">
-                    <input class="form-control osk-trigger input-lg" type="number" id="remoteSStime" name="features[remoteSStime]" value="<?=$this->remoteSStime ?>" data-trigger="change" min="-1" max="100" placeholder="-1" />
-                    <span class="help-block">Sets the activation time for the remote screensaver (0-100 seconds, -1 disables the feature)</span>
-                </div>
-            </div>enha*/ ?>
             <div class="form-group">
                 <label for="udevil" class="control-label col-sm-2">USB Automount</label>
                 <div class="col-sm-10">
@@ -442,16 +318,6 @@
                     <span class="help-block">Toggle automount for USB drives</span>
                 </div>
             </div>
-<?php /*enha            <div class="form-group">
-                <label for="coverart" class="control-label col-sm-2">Display album cover</label>
-                <div class="col-sm-10">
-                    <label class="switch-light well" onclick="">
-                        <input name="features[coverart]" type="checkbox" value="1"<?php if($this->coverart == 1): ?> checked="checked" <?php endif ?>>
-                        <span><span>OFF</span><span>ON</span></span><a class="btn btn-primary"></a>
-                    </label>
-                    <span class="help-block">Toggle the display of album art on the Playback main screen</span>
-                </div>
-            </div>enha*/ ?>
             <div <?php if($this->lastfm['enable'] === '1'): ?>class="boxed-group"<?php endif ?> id="lastfmBox">
                 <div class="form-group">
                     <label for="lastfm" class="control-label col-sm-2">Last.fm scrobbling</label>
@@ -480,7 +346,6 @@
                     </div>
                 </div>
             </div>
-<?php /*xxx            <div <?php if($this->samba['enable'] === '1'): ?>class="boxed-group"<?php endif ?> id="sambaBox">xxx*/ ?>
                 <div class="form-group">
                     <label for="samba" class="control-label col-sm-2">Samba File-Server</label>
                     <div class="col-sm-10">
@@ -491,22 +356,6 @@
                         <span class="help-block">Enable Samba to share your music files on your network</span>
                     </div>
                 </div>
-<?php /*xxx                <div class="<?php if($this->samba['enable'] != 1): ?>hide<?php endif ?>" id="sambaAuth">
-                    <div class="form-group">
-                        <label class="control-label col-sm-2" for="samba-readwrite">Read/Write access</label>
-                        <div class="col-sm-10">
-							<label class="switch-light well" onclick="">
-								<input id="readwrite" name="features[samba][readwrite]" type="checkbox" value="1"<?php if($this->samba['readwrite'] === '1'): ?> checked="checked" <?php endif ?>>
-								<span><span>OFF</span><span>ON</span></span><a class="btn btn-primary"></a>
-							</label>
-                            <span class="help-block">Choose Read-Only access (<strong>OFF</strong>) or Read/Write access (<strong>ON</strong>).<br>
-							By default <strong>no passwords</strong> are required to access the files. Note: Critical system files can be modified by <strong>anyone on your network</strong> after setting read/write access ON.<br>
-							Read/Write access allows you to update your music library over the network, it is convenient, but not very fast.<br>
-							You can modify the Samba configuration via your PC after setting Samba read/write access ON. Some instructions are included in the files</span>
-                        </div>
-                    </div>
-                </div>
-            </div>xxx*/ ?>
             <div class="form-group form-actions">
                 <div class="col-sm-offset-2 col-sm-10">
                     <button class="btn btn-primary btn-lg" value="1" name="features[submit]" type="submit">apply settings</button>
@@ -514,52 +363,6 @@
             </div>
         </fieldset>
     </form>
-<?php /*addo    <form class="form-horizontal" method="post">
-        <fieldset>
-            <legend>Backup / Restore configuration</legend>
-            <p>Transfer settings between multiple RuneAudio installations, saving time during new/upgrade installations.</p>
-            <div class="form-group">
-                <label class="control-label col-sm-2">Backup player config</label>
-                <div class="col-sm-10">
-                    <input class="btn btn-primary btn-lg" type="submit" name="syscmd" value="backup" id="syscmd-backup">
-					<span class="help-block">Export a compressed archive containing all the settings of this player.</span>
-                </div>
-            </div>
-        </fieldset>
-    </form>
-    <form class="form-horizontal" method="post">
-        <fieldset>
-            <div class="form-group">
-                <label class="control-label col-sm-2">Activate Restore</label>
-                <div class="col-sm-10">
-                    <input class="btn btn-primary btn-lg" type="submit" name="syscmd" value="activate" id="syscmd-activate">
-					<span class="help-block">For security reasons restore must be activated before use</span>
-                </div>
-            </div>
-        </fieldset>
-    </form>
-    <form class="form-horizontal" id="restore">
-        <fieldset>
-			<div <?php if($this->restoreact != 1): ?>hidden<?php endif ?>>
-				<div class="form-group">
-					<label class="control-label col-sm-2">Restore player config</label>
-					<div class="col-sm-10">
-						<p>
-							<span id="btn-backup-browse" class="btn btn-default btn-lg btn-file">
-								Browse... <input type="file" name="filebackup">
-							</span> 
-							<span id="backup-file"></span>
-							<span class="help-block">Restore a previously exported backup.<br>
-							<strong>The system will reboot</strong> after restoring the backup.<br>
-							<strong>Tip:</strong> Make a new backup after checking and correcting each restore.
-							Otherwise information concerning any new feature will be missing and the feature will be automatically switched off</span>
-						</p>
-						<button id="btn-backup-upload" name="syscmd" value="restore" class="btn btn-primary btn-lg" disabled>Restore</button>
-					</div>
-				</div>
-			</div>
-		</fieldset>
-    </form>addo*/ ?>
 </div>
 <div id="modal-sysinfo" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-sysinfo-label" aria-hidden="true">
     <div class="modal-dialog">
