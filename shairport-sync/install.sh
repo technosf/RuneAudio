0caeef3cd1f4c16ce7623ba841e3808a813608d5
rern <rernrern@gmail.com>
2019-08-07 22:17:15 +0700

    Update install.sh

--- shairport-sync/install.sh
+++ shairport-sync/install.sh
@@ -18,10 +18,6 @@ rm /etc/shairport-sync.conf \
 	/usr/lib/systemd/system/shairport-sync.service
 
 # install with compiled packaged --with-metadata
-wget -qN https://github.com/rern/RuneAudio/raw/master/shairport-sync/shairportmeta.php -P /srv/http
-wget -qN https://github.com/rern/RuneAudio/raw/master/shairport-sync/shairportstartstop.sh -P /srv/http
-chown http:http /srv/http/shairport*
-chmod 755 /srv/http/shairport*
 file=shairport-sync-3.3.1-1-armv7h.pkg.tar.xz
 wget -qN https://github.com/rern/RuneAudio/raw/master/shairport-sync/$file
 pacman -U --noconfirm $file
@@ -29,6 +25,7 @@ rm $file
 
 sed -i 's/\(^User=\).*/\1http/; s/\(^Group=\).*/\1http/' /usr/lib/systemd/system/shairport-sync.service
 
+wget -qN https://github.com/rern/RuneAudio/raw/master/shairport-sync/shairportmeta.php -P /srv/http
 # metadata unit file
 cat << 'EOF' > /etc/systemd/system/shairport-meta.service
 [Unit]
@@ -56,6 +53,22 @@ alsa = {
 	output_device = "hw:0";
 }
 EOF
+
+cat << 'EOF' > /srv/http/shairportstartstop.sh
+#!/bin/bash
+
+if (( $# > 0 )); then
+	systemctl start mpd mpdidle
+	redis-cli set activePlayer MPD
+	curl -s -X POST 'http://localhost/pub?id=airplay' -d 0
+else
+	systemctl stop mpd mpdidle
+	redis-cli set activePlayer AirPlay
+	curl -s -X POST 'http://localhost/pub?id=airplay' -d 1
+fi
+EOF
+chown http:http /srv/http/shairport*
+chmod 755 /srv/http/shairport*
 
 systemctl daemon-reload
 [[ $active ]] && systemctl restart shairport-sync shairport-meta
