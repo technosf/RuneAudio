8e113eba07333d6a082c75cc2cf479247da34d7b
rern <rernrern@gmail.com>
2019-08-07 20:55:38 +0700

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -21,8 +21,6 @@ ln -s /lib/libip4tc.so.{2.0.0,0}
 rm $file
 wgetnc https://github.com/rern/RuneAudio/raw/master/nginx/nginx.conf -P /etc/nginx
 wgetnc https://github.com/rern/RuneAudio/raw/master/nginx/50x.html -P /etc/nginx/html
-chown http:http /srv/http/50x.html
-rm -r /etc/nginx/html
 
 echo -e "$bar Restart NGINX ..."
 
