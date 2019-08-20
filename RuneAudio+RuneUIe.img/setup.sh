26a196fc4acb71bb711c420e485c3a72b30654d7
rern <rernrern@gmail.com>
2019-08-20 03:09:33 -0700

    Update setup.sh

--- RuneAudio+RuneUIe.img/setup.sh
+++ RuneAudio+RuneUIe.img/setup.sh
@@ -17,9 +17,8 @@ mpc -q volume 50; mpc -q repeat 0; mpc -q random 0; mpc -q single 0; mpc -q cons
 mpc clear
 redis-cli del mpddb &> /dev/null
 
-echo -e "\n$bar Reset Redis ..."
-redis-cli save &> /dev/null
-cp /srv/http/assets/img/redis/* /var/lib/redis
+echo -e "\n$bar Reset MPD Databas ..."
+rm /var/lib/d/mpd.db
 
 echo -e "\n$bar Clear packages cache ..."
 rm -f /var/cache/pacman/pkg/*
