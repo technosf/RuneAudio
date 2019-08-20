0292fb67e5ebf4b22d5243d786c19516f051f383
rern <rernrern@gmail.com>
2019-08-20 02:25:00 -0700

    Update runonce.sh

--- RuneAudio+RuneUIe.img/runonce.sh
+++ RuneAudio+RuneUIe.img/runonce.sh
@@ -3,9 +3,11 @@
 rm $0
 
 # expand partition
-echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
-partprobe /dev/mmcblk0
-resize2fs /dev/mmcblk0p2
+if [[ $( sfdisk -F /dev/mmcblk0 | head -n1 | awk '{print $6}' ) > 0 ]]; then
+	echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
+	partprobe /dev/mmcblk0
+	resize2fs /dev/mmcblk0p2
+fi
 
 # wait for usb/nas drive mounted
 while $( sleep 1 ); do
