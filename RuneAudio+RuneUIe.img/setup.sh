1ff986f5575d147fc2267e82aa856beb0120330d
rern <rernrern@gmail.com>
2019-08-22 07:30:39 +0700

    Update setup.sh

--- RuneAudio+RuneUIe.img/setup.sh
+++ RuneAudio+RuneUIe.img/setup.sh
@@ -6,27 +6,27 @@ rm $0
 
 title -l '=' "$bar Rune0.5+RuneUIe Reset ..."
 
-echo -e "\n$bar Delete unnecessary files ..."
-rm -f /etc/netctl/test
-rm -f /srv/http/assets/css/*.old
-rm -f /srv/http/assets/js/*.orig
-rm -rf /srv/http/assets/less
-
 echo -e "\n$bar Reset MPD settings ..."
+
 mpc -q volume 50; mpc -q repeat 0; mpc -q random 0; mpc -q single 0; mpc -q consume 0
 mpc clear
+rm -f /var/lib/mpd/mpd.db
 redis-cli del mpddb &> /dev/null
 
-echo -e "\n$bar Reset MPD Database ..."
-systemctl stop mpd
-rm /var/lib/mpd/mpd.db
-systemctl start mpd
+echo -e "\n$bar Reset Database ..."
+
+redis-cli save
+redis-cli savebg
+cp /srv/http/assets/img/redis/* /var/lib/redis
+sed -i -e '\|^dir /srv/http/assets/img/redis/| s|^|#|' -e '\|^#dir /var/lib/redis/| s|^#||' /etc/redis.conf
 
 echo -e "\n$bar Clear packages cache ..."
+
 rm -f /var/cache/pacman/pkg/*
 rm -rf /srv/http/.cache/chromium/Default/*
 
 echo -e "\n$bar Clear mountpoints ..."
+
 mounts=( $( ls -d /mnt/MPD/NAS/*/ 2> /dev/null ) )
 if (( ${#mounts[@]} > 0 )); then
 	for mount in "${mounts[@]}"; do
@@ -42,10 +42,12 @@ if (( ${#mounts[@]} > 0 )); then
 fi
 
 echo -e "\n$bar Reset mirrorlist ..."
+
 rm /etc/pacman.d/*
 wgetnc https://github.com/archlinuxarm/PKGBUILDs/raw/master/core/pacman-mirrorlist/mirrorlist -P /etc/pacman.d
 
 echo -e "\n$bar Startup setup script ..."
+
 wgetnc https://github.com/rern/RuneAudio/raw/master/RuneAudio%2BRuneUIe.img/runonce.sh -P /root
 chmod +x /root/runonce.sh
 
