8aad472417fa2a6c1a95a8f666bb69ced39d78d4
rern <rernrern@gmail.com>
2019-08-31 17:03:10 +0700

    Update runonce.php

--- RuneAudio+R_e1.img/runonce.php
+++ RuneAudio+R_e1.img/runonce.php
@@ -1,5 +1,5 @@
 <script src="assets/js/vendor/jquery-2.1.0.min.js"></script>
-<script src="assets/js/addonsinfo.<?=$time?>.js"></script>
+<script src="assets/js/info.<?=$time?>.js"></script>
 <script>
 	$.post( 'commands.php', { bash: [
 		  "sed -i '/runonce.php/ d' /srv/http/indexbody.php"
