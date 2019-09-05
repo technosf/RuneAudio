8110f385b21ec1d910082b73184382540020765e
rern <rernrern@gmail.com>
2019-09-05 16:53:39 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -76,7 +76,7 @@ echo $file
 string=$( cat <<'EOF'
 	<a id="aria2">
 		<img src="/assets/img/addons/thumbaria.png" style="filter: brightness(1.3)">Aria2
-		<i id="setting-aria2" class="fa fa-rune submenu settings"></i>
+		<i id="setting-aria2" class="fa fa-gear submenu settings"></i>
 	</a>
 EOF
 )
