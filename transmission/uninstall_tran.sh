cd6abbdbf60b077ba20978b2a526cff3aabbadca
rern <rernrern@gmail.com>
2019-09-27 10:38:33 +0700

    Update uninstall_tran.sh

--- transmission/uninstall_tran.sh
+++ transmission/uninstall_tran.sh
@@ -18,7 +18,8 @@ echo -e "$bar Remove files ..."
 
 rm -rv /etc/systemd/system/transmission.service.d
 dirweb=/usr/share/transmission/web
-rm -rv $dirweb/tr-web-control $dirweb/{favicon.ico,index.html,index.mobile.html}
+rm -r $dirweb/tr-web-control
+rm -v $dirweb/{favicon.ico,index.html,index.mobile.html}
 mv $dirweb/index{.original,}.html
 
 uninstallfinish $@
