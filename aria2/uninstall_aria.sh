9bccbbdf278fa0b2beb10ae93a75fcbf8a50ecdb
rern <rernrern@gmail.com>
2019-10-06 17:12:04 +0700

    Update uninstall_aria.sh

--- aria2/uninstall_aria.sh
+++ aria2/uninstall_aria.sh
@@ -11,10 +11,6 @@ systemctl disable --now aria2
 rm -v /etc/systemd/system/aria2.service
 systemctl daemon-reload
 
-# restore file
-echo -e "$bar Restore files ..."
-restorefile /srv/http/indexbody.php /srv/http/assets/js/main.js
-
 # remove files #######################################
 echo -e "$bar Remove files ..."
 
