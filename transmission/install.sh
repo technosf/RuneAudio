ddfe5d298e5e3a59d26e1b07dc022163148989ca
rern <rernrern@gmail.com>
2019-11-22 13:50:56 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -14,7 +14,7 @@ getuninstall
 
 [[ ! -e /usr/bin/transmission-cli ]] && pacman -Sy --noconfirm transmission-cli
 
-mnt=$( mount | grep /dev/sda1 | cut -d' ' -f3 )
+mnt=$( df | grep /dev/sd | grep -v /$ | tail -1 | cut -d' ' -f1 )
 path="$mnt/transmission"
 if [[ ! -e "$path" ]]; then
 	mkdir -p "$path"
