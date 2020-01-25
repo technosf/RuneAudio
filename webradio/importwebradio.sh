8ade41f0b097744a86b501c9b6d9eb52864f3e9e
rern <rernrern@gmail.com>
2020-01-25 09:36:13 +0700

    Create importwebradio.sh

---
+++ webradio/importwebradio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm $0
+
+. /srv/http/addons-functions.sh
+
+# all files include sub-directory
+files=$( find /mnt/MPD/Webradio -type f 2> /dev/null )
+if [[ -z $files ]]; then
+	title -l '=' "$info No webradio files found."
+	title -nt 'Copy *.pls to /mnt/MPD/Webradio/ then run again.'
+	exit
+fi
+
+readarray -t files <<<"$files"
+for file in "${files[@]}"; do
+	name="${file%.*}"
+	url=$( grep '^File' "$file" | cut -d '=' -f2 )
+	printf "%-30s : $url\n" "$name"
+	echo $name /srv/http/data/webradios/${url//\//|}
+done
+
+title -l '=' "$bar Webradio imported successfully."
